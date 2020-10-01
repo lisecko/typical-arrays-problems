0a2fe2b73b2abffe7467ba713d5477a110369f88
Uladzislau Lisetski <lisetskiywlad@gmail.com>
2020-10-01 15:50:47 +0300

    refactor: add space in 9 line

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  let min = array[0];
+
+  if(array.length === 0) return 0
+
+  for(let i = 0; i < array.length; i++)
+  {
+    
+    if(array[i] < min)
+    {
+      min = array[i]
+    }
+  }
+
+  return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  let max = array[0];
+
+  if(array.length === 0) return 0
+
+  for(let i = 0; i < array.length; i++)
+  {
+    if(array[i] > max)
+    {
+      max = array[i]
+    }
+  }
+  return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  let sum = 0
+  let result = 0
+
+  if(array.length === 0) return 0
+
+  for(let i = 0; i < array.length; i++)
+  {
+    sum += array[i]
+  }
+
+  result = sum / array.length;
+  return result
 }
